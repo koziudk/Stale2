afa3c32d19f48981a58e1e6ca4bcb20cd8f6f21a
koziudk <koziudk@gmail.com>
2017-10-22 17:43:48 +0200

    drugi commit

--- src/Stale.java
+++ src/Stale.java
@@ -4,10 +4,10 @@ public class Stale {
 	public static void main(String[] args) 
 	{
 		// TODO Auto-generated method stub
-final double CM_PER_INCH = 2.54;
-double paperWidth = 8.5;
-double paperHeight = 11;
-System.out.println("Rozmiary papieru w centymetrach: "
+		final double CM_PER_INCH = 2.54;
+		double paperWidth = 8.5;
+		double paperHeight = 11;
+		System.out.println("Rozmiary papieru w centymetrach: "
 		+paperWidth*CM_PER_INCH + " na " +paperHeight*CM_PER_INCH);
 	}
 
